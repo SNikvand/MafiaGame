26bf68061eb2c1a0cce01a95debb6e535f2dd66f
snikvand <shahin.nikvand@gmail.com>
2014-03-12 17:12:30 -0700

    General Notes ----------------- Added Bootstrap Files.
    app.js
    -------
    Created a Section for routes to be added. (readability purposes)

--- app.js
+++ app.js
@@ -4,10 +4,15 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+
+//ALL PAGE ROUTES\\
+
+var routes = require('./routes');
+var user = require('./routes/user');
+
+//END PAGE ROUTES\\
 
 var app = express();
 
