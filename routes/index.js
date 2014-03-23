b7c8f8cf40d3786dedf822d94bba0559e9a9cd5f
snikvand <shahin.nikvand@gmail.com>
2014-03-23 13:02:39 -0700

    auto filled avatars based on the directory (for index page)
    Completed.
    By: A. Wei.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@
  * GET home page.
  */
 
+var files = require('fs').readdirSync('public/images/avatars');
+
 exports.index = function (req, res) {
 
-    res.render('index', {title: 'Express'});
+    res.render('index', {files: files});
 };
