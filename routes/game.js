b526be75f0267b6f9c0d3aceb5c179bdf9358746
snikvand <shahin.nikvand@gmail.com>
2014-03-26 15:22:59 -0700

    backend username validation completed.

--- routes/game.js
+++ routes/game.js
@@ -1,10 +1,35 @@
-/**
- * Created by shahin on 20/03/14.
- */
+/*--------------------------------------------------------------------------------
+ purpose: simply validate the username. this item will return a -1 on failure.
+
+ created by: AJ, Sabrina, Emanual, Shahin
+ date/modified: Web March 26th 2014
+
+ version: 1.0
+
+ peer reviewed by: Shahin
+ -------------------------------------------------------------------------------*/
+function validateName(username) {
+    if (username.length < 3 || username.length > 15) {
+        return -1;
+    }
+
+    for (var i = 0; i < username.length; i++) {
+        var char1 = username.charAt(i);
+        var cc = char1.charCodeAt(0);
+        if (!((cc > 47 && cc < 58) || (cc > 64 && cc < 91) || (cc > 96 && cc < 123))) {
+            return -1;
+        }
+    }
+}
 
 exports.index = function(req, res) {
     var avatarNum = req.body.avatar;
     var Username = req.body.username;
+
+    if(validateName(Username) == -1) {
+        res.redirect('#');
+    }
+
     var output = {
         name: Username,
         avatar: avatarNum
